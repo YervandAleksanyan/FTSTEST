f30cf6c0d429394e7f7fa5bbea6f9e3dac6ed8b8
YervandAlexanyan <yervandalexanyan@gmail.com>
2018-11-16 17:06:48 +0400

    Navigation item value transform animation completed

--- app/src/main/java/com/example/yervand/ftstest/utill/CodexNavigationItem.kt
+++ app/src/main/java/com/example/yervand/ftstest/utill/CodexNavigationItem.kt
@@ -3,31 +3,27 @@ package com.example.yervand.ftstest.utill
 import android.animation.*
 import android.content.Context
 import android.util.AttributeSet
-import android.util.Log
 import android.view.View
-import android.view.ViewPropertyAnimator
 import android.widget.FrameLayout
 import android.widget.ProgressBar
 import android.widget.TextView
 import com.example.yervand.ftstest.R
 import android.view.animation.AccelerateDecelerateInterpolator
-import android.view.animation.Animation
-import android.view.animation.TranslateAnimation
-import androidx.transition.TransitionManager
 import com.robertlevonyan.components.kex.onEnd
-import android.view.animation.ScaleAnimation
-import android.R.attr.y
-import android.R.attr.x
 
 
 class CodexNavigationItem : FrameLayout {
+
+    enum class ScrollDirection {
+        DOWN,
+        UP
+    }
+
     private lateinit var progressBar: ProgressBar
-    private lateinit var itemText: TextView
-    private lateinit var itemTextSecond: TextView
-    private lateinit var currentText: TextView
+    private lateinit var mainTextView: TextView
+    private lateinit var secondTextView: TextView
     private lateinit var progressAnimation: ValueAnimator
     private var progressValue: Int? = 0
-    private val circularAnimation = AnimatorSet()
 
     constructor(context: Context) : super(context) {
         init(context, null)
@@ -48,10 +44,9 @@ class CodexNavigationItem : FrameLayout {
 
     private fun initViews() {
         progressBar = findViewById(R.id.progress_bar)
-        itemText = findViewById(R.id.progress_value)
-        itemTextSecond = findViewById(R.id.progress_value_second)
-        itemTextSecond.visibility = View.INVISIBLE
-        currentText = itemTextSecond
+        mainTextView = findViewById(R.id.progress_value)
+        secondTextView = findViewById(R.id.progress_value_second)
+        secondTextView.visibility = View.INVISIBLE
         initProgressAnimation()
     }
 
@@ -67,91 +62,65 @@ class CodexNavigationItem : FrameLayout {
 
     fun setItemText(text: String, scrollDirection: ScrollDirection) {
 
-        if (itemText.text != text) {
-            val auxLabelOffset: Double = when (scrollDirection) {
-                ScrollDirection.UP -> -progressBar.height / 5.0
-                else -> progressBar.height / 5.0
+        if (mainTextView.text != text) {
+            val auxLabelOffset: Float = when (scrollDirection) {
+                ScrollDirection.DOWN -> mainTextView.height / 2.0F
+                else -> -mainTextView.height / 2.0F
             }
 
-            itemTextSecond.text = text
-            itemTextSecond
-                    .animate()
-                    .translationY(auxLabelOffset.toFloat())
-                    .scaleX(1F)
-                    .scaleY(0.1F)
-                    .start()
+            //Second View Animation
+            secondTextView.text = text
+            secondTextView.translationY = auxLabelOffset
+            secondTextView.scaleY = 0.1F
+            secondTextView.scaleX = 1F
+            secondTextView.visibility = View.VISIBLE
 
-            itemTextSecond.visibility = View.VISIBLE
+            //Main View animation
+            val translationAnim2 = ObjectAnimator.ofFloat(mainTextView, "translationY", -auxLabelOffset)
+                    .apply {
+                        duration = 500
+                    }
 
-            val animation: ViewPropertyAnimator = itemText.animate()
-                    .setDuration(500)
-                    .translationY(-auxLabelOffset.toFloat())
-                    .scaleX(1F)
-                    .scaleY(0.1F)
-            animation.start()
-            animation.setListener(object : Animator.AnimatorListener {
-                override fun onAnimationRepeat(animation: Animator?) {
+            val scaleYAnim2 = ObjectAnimator.ofFloat(mainTextView, "scaleY", 0.1F)
+                    .apply {
+                        duration = 500
+                    }
+            val scaleXAnim2 = ObjectAnimator.ofFloat(mainTextView, "scaleX", 1F)
+                    .apply {
+                        duration = 500
+                    }
+            val translationAnim3 = ObjectAnimator.ofFloat(secondTextView, "translationY", 0F)
+                    .apply {
+                        duration = 500
+                    }
 
-                }
-
-                override fun onAnimationEnd(animation: Animator?) {
-                    itemText.text = itemTextSecond.text
-                    itemText
-                            .animate()
-                            .scaleX(1F)
-                            .scaleY(1F)
-                            .translationY(0F)
-                            .start()
-                    itemTextSecond.visibility = View.INVISIBLE
-                }
-
-                override fun onAnimationCancel(animation: Animator?) {
-                }
-
-                override fun onAnimationStart(animation: Animator?) {
-                    itemTextSecond
-                            .animate()
-                            .translationY(0F)
-                            .scaleX(1F)
-                            .scaleY(0.1F)
-                            .start()
-                }
-            })
+            val scaleYAnim3 = ObjectAnimator.ofFloat(secondTextView, "scaleY", 1F)
+                    .apply {
+                        duration = 500
+                    }
+            val scaleXAnim3 = ObjectAnimator.ofFloat(secondTextView, "scaleX", 1F)
+                    .apply {
+                        duration = 500
+                    }
+            val transform = AnimatorSet()
+            transform.interpolator = AccelerateDecelerateInterpolator()
+            transform.playTogether(
+                    translationAnim2,
+                    scaleXAnim2,
+                    scaleYAnim2,
+                    translationAnim3,
+                    scaleXAnim3,
+                    scaleYAnim3
+            )
+            transform.start()
+            transform.onEnd {
+                mainTextView.text = secondTextView.text
+                mainTextView.translationY = 0F
+                mainTextView.scaleY = 1F
+                mainTextView.scaleX = 1F
+                secondTextView.visibility = View.INVISIBLE
+            }
         }
-
-//        val upAnimation: ObjectAnimator?
-//        val downAnimation: ObjectAnimator?
-//        if (!text.isEmpty()) {
-//            restoreViewsAlpha()
-//            if (currentText == itemText) {
-//                itemText.text = text
-//                currentText = itemTextSecond
-//            } else {
-//                itemTextSecond.text = text
-//                currentText = itemText
-//            }
-//
-//            if (currentText.text != text) {
-//                when (scrollDirection) {
-//                    ScrollDirection.UP -> {
-//                        upAnimation = createTranslationAnimation(currentText, 0F, progressBar.height.toFloat())
-//
-//                        downAnimation = createTranslationAnimation(if (currentText == itemText) itemTextSecond else itemText,
-//                                -progressBar.height.toFloat(), 0F)
-//
-//                    }
-//                    else -> {
-//                        upAnimation = createTranslationAnimation(currentText, 0F, -progressBar.height.toFloat())
-//
-//                        downAnimation = createTranslationAnimation(if (currentText == itemText) itemTextSecond else itemText, progressBar.height.toFloat(), 0F)
-//                    }
-//                }
-//                createCircularAnimation(scrollDirection, upAnimation, downAnimation)
-//            }
-//        } else {
-//            textAlphaAnimation(itemTextSecond)
-//            textAlphaAnimation(itemText)
-//        }
     }
 
     private fun initProgressAnimation() {
@@ -172,40 +141,10 @@ class CodexNavigationItem : FrameLayout {
                         duration = 1000
                     }
 
-    private fun createCircularAnimation(scrollDirection: ScrollDirection, vararg translationAnimation: ObjectAnimator) {
-        circularAnimation.duration = 1000
-        circularAnimation.playTogether(translationAnimation.asList())
-        circularAnimation.start()
-        circularAnimation.onEnd {
-            currentText.translationY =
-                    if (ScrollDirection.UP == scrollDirection) progressBar.height.toFloat()
-                    else -progressBar.height.toFloat()
-        }
-    }
 
     fun progressAnimationStop() {
         progressAnimation.pause()
     }
 
-    enum class ScrollDirection {
-        DOWN,
-        UP
-    }
 
-    private fun textAlphaAnimation(view: TextView) {
-        val animation = ObjectAnimator.ofFloat(view, "alpha", 1F, 0F)
-                .apply {
-                    duration = 1000
-                    interpolator = AccelerateDecelerateInterpolator()
-                }
-        animation.onEnd {
-            view.text = ""
-        }
-        animation.start()
-    }
-
-    private fun restoreViewsAlpha() {
-        itemText.alpha = 1F
-        itemTextSecond.alpha = 1F
-    }
 }
